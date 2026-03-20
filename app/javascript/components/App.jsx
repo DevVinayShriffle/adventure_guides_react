8d171df5b8c38711b1814e6e09ebd291e15e43f7
DevVinayShriffle <vinayks@shriffle.com>
2026-03-20 12:38:49 +0530

    first commit

---
+++ app/javascript/components/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="bg-red-400">Hello from react !!! doing changes to check </h1>
+    </div>
+  )
+};
+
+export default App;  
---
+++ config/routes.rb
@@ -0,0 +1,49 @@
+Rails.application.routes.draw do
+
+  # require 'sidekiq/web'
+  # mount Sidekiq::Web => '/sidekiq'
+  
+  root "destinations#index"
+  # devise_for :users, path: '', path_names: {
+  #   sign_in: 'login',
+  #   sign_out: 'logout',
+  #   registration: 'signup'
+  # }, controllers: {
+  #   registrations: 'users/registrations',
+  #   sessions: 'users/sessions',
+  #   passwords: 'users/passwords'
+  # }
+
+  resources :destinations, only: [:index, :show] do
+    resources :buses, only: [:index]
+  end
+
+  resources :buses, only: [:index, :show]
+
+  resources :schedules, only: [:index, :show]
+
+  resources :bookings, only: [:create, :index, :show, :new] do
+    patch :cancel, on: :member
+
+    collection do
+      get :upcoming
+      get :cancelled
+    end
+  end
+
+  namespace :admin do
+    resources :destinations
+    get :users
+  end
+
+  namespace :guide do
+    resources :buses do
+      resources :bus_stops
+      # resources :schedules
+    end
+    resources :schedules
+  end
+
+  get '/dashboard', to: 'dashboards#index', as: 'dashboard'
+
+end
