29ce5b5b5092faed8642a005b680e67697ff9d54
DevVinayShriffle <vinayks@shriffle.com>
2026-03-23 19:47:10 +0530

    Added basic design of signup form

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 require_dependency 'user_serializer'
 class Users::RegistrationsController < Devise::RegistrationsController
   respond_to :json, :html, :turbo_stream
-
+  # byebug
   def destroy
     if current_user.destroy
       respond_to do |format|
@@ -15,63 +15,65 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def respond_with(resource, _opts = {})
     return if request.get?
-    # byebug
+    byebug
     return if (resource.errors.any? && action_name == "update")
     if resource.persisted?
       @token = request.env['warden-jwt_auth.token']
       headers['Authorization'] = @token
       
       # if resource[:updated_at] == Time.now
-      respond_to do |format|
+      # respond_to do |format|
 
-        # SIGNUP → REDIRECT
-        if action_name == "create"
-          flash[:notice] = "User Registered successfully."
-          format.html { redirect_to dashboard_path }
-          format.turbo_stream { redirect_to dashboard_path }
-        end
+      #   # SIGNUP → REDIRECT
+      #   if action_name == "create"
+      #     flash[:notice] = "User Registered successfully."
+      #     format.html { redirect_to dashboard_path }
+      #     format.turbo_stream { redirect_to dashboard_path }
+      #   end
 
-        # UPDATE → TURBO STREAM
-        if action_name == "update"
-          flash.now[:notice] = "Profile updated successfully."
-          format.turbo_stream do
-            render turbo_stream: [
-              turbo_stream.replace(
-                "sidebar_avatar",
-                partial: "dashboards/sidebar_avatar",
-                locals: { user: current_user }
-              ),
-              turbo_stream.replace("flash-messages", partial: "shared/flash_messages"),
-              turbo_stream.update(
-                "dashboard_content",
-                render_to_string(
-                  inline: %{
-                    <h2 class="text-2xl font-semibold text-gray-600">
-                      Welcome #{current_user.name}
-                    </h2>
-                    <p class="mt-2 text-gray-500">
-                      Profile updated successfully.
-                    </p>
-                  }
-                )
-              )
-            ]
-          end
+      #   # UPDATE → TURBO STREAM
+      #   if action_name == "update"
+      #     flash.now[:notice] = "Profile updated successfully."
+      #     format.turbo_stream do
+      #       render turbo_stream: [
+      #         turbo_stream.replace(
+      #           "sidebar_avatar",
+      #           partial: "dashboards/sidebar_avatar",
+      #           locals: { user: current_user }
+      #         ),
+      #         turbo_stream.replace("flash-messages", partial: "shared/flash_messages"),
+      #         turbo_stream.update(
+      #           "dashboard_content",
+      #           render_to_string(
+      #             inline: %{
+      #               <h2 class="text-2xl font-semibold text-gray-600">
+      #                 Welcome #{current_user.name}
+      #               </h2>
+      #               <p class="mt-2 text-gray-500">
+      #                 Profile updated successfully.
+      #               </p>
+      #             }
+      #           )
+      #         )
+      #       ]
+      #     end
 
-          format.html { redirect_to dashboard_path }
-        end
+      #     format.html { redirect_to dashboard_path }
+      #   end
 
-        format.json do
-          render json: {
-            status: {
-              code: 200,
-              message: "Success",
-              token: "Bearer #{@token}",
-              data: { user: UserSerializer.new(resource) }
-            }
-          }
-        end
-      end
+      #   format.json do
+      #     render json: {
+      #       status: {
+      #         code: 200,
+      #         message: "Success",
+      #         token: "Bearer #{@token}",
+      #         data: { user: UserSerializer.new(resource) }
+      #       }
+      #     }
+      #   end
+      # end
+
+      render json: {status: {code: 200, message: "Success", token: "Bearer #{@token}", data: { user: UserSerializer.new(resource) }}}
     else
       render :new, status: :unprocessable_entity
     end
--- app/javascript/components/auth/SignUp.jsx
+++ app/javascript/components/auth/SignUp.jsx
@@ -1,9 +1,136 @@
-import React from 'react'
+import React from 'react';
+import {useState} from 'react'
+import axios from 'axios'
 
 function SignUp() {
+  const[formData, setFormData]=useState(
+    {
+      name: '',
+      email: '',
+      password: '',
+      password_confirmation: '',
+      phone: '',
+      avatar: '',
+      role: 'tourist'
+    }
+  )
+
+  function handleInput(e) {
+    const{name, value}=e.target
+    setFormData((pre)=>({...pre, [name]: value}))
+  }
+
+  const handleSubmit=async (e)=>{
+    console.log(formData)
+    e.preventDefault()
+    try {
+      const response=await axios.post('/api/v1/users/signup', {
+        user: formData
+      })
+      console.log('Signup Successful: ', response.data);
+    }catch(error){
+      console.error('signup error: ', error.response?.data || error.message)
+    }
+  }
+
   return (
     <>
-      <h1>This is signup page</h1>
+      <div className='w-full mt-10 px-[30%]'>
+        <form onSubmit={handleSubmit} className="border-2 border-black p-2 rounded flex flex-col gap-2">
+          <h2 className='m-auto'>Sign Up</h2>
+
+          {/* Name Input */}
+          <div className="flex justify-between">
+            <label htmlFor="name">Name:</label>
+            <input
+              className='border-1 border-black rounded'
+              type="text"
+              id="name"
+              name="name"
+              required
+              autoFocus
+              onChange={handleInput}
+            />
+          </div>
+
+          {/* Email Input */}
+          <div className="flex justify-between">
+            <label htmlFor="email">Email:</label>
+            <input
+            className='border-1 border-black rounded'
+              type="email"
+              id="email"
+              name="email"
+              required
+              onChange={handleInput}
+            />
+          </div>
+
+          {/* Password Input */}
+          <div className="flex justify-between">
+            <label htmlFor="password">Password:</label>
+            <input
+            className='border-1 border-black rounded'
+              type="password"
+              id="password"
+              name="password"
+              required
+              minLength="6"
+              onChange={handleInput}
+            />
+          </div>
+
+          {/* Password confirmation Input */}
+          <div className="flex justify-between">
+            <label htmlFor="password_confirmation">Confirm Password:</label>
+            <input
+            className='border-1 border-black rounded'
+              type="password"
+              id="password_confirmation"
+              name="password_confirmation"
+              required
+              minLength="6"
+              onChange={handleInput}
+            />
+          </div>
+
+          {/* Phone Number Input */}
+          <div className="flex justify-between">
+            <label htmlFor="phone_no">Phone Number:</label>
+            <input
+            className='border-1 border-black rounded'
+              type="tel"
+              id="phone"
+              name="phone"
+              onChange={handleInput}
+            />
+          </div>
+
+          {/* Avatar Input (File) */}
+          <div className="flex justify-between">
+            <label htmlFor="avatar">Avatar:</label>
+            <input
+            className='border-1 border-black rounded w-63'
+              type="file"
+              id="avatar"
+              name="avatar"
+              accept="image/png, image/jpeg" 
+              onChange={handleInput}
+            />
+          </div>
+
+          {/* Role Input (Dropdown/Select) */}
+          <div className="flex justify-between">
+            <label htmlFor="role">Role:</label>
+            <select id="role" name="role" onChange={handleInput} className='border-1 border-black w-63 rounded'>
+              <option value="tourist" selected>Tourist</option>
+              <option value="guide">Guide</option>
+            </select>
+          </div>
+
+          <button className='bg-black text-white px-3 py-2 rounded w-50 m-auto' type="submit">Sign Up</button>
+        </form>
+      </div>
     </>
   )
 }
